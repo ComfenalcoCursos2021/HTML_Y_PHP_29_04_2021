d4b806e42c4aec03db268222dbd38de9a0cb4b6b
Miguel Angel Castro Escamilla <ma98030965140@gmail.com>
2021-04-29 16:25:33 -0500

    29 - 04 - 2021

---
+++ formulario.php
@@ -0,0 +1,15 @@
+<?php
+
+
+    echo <<<NOMBRE
+    Tu nombre es: $_REQUEST[nombre] <br>
+NOMBRE;
+    echo <<<EDAD
+    Tu edad es: $_REQUEST[edad] <br>
+EDAD;
+    echo <<<GENERO
+    Tu genero es: $_REQUEST[Genero] <br>
+GENERO;
+
+
+?>
